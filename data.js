0cdd0ea29695dba27c568e2ae731c0ae9571a3e1
Efe Atogun <50411930+ATOGUNEFE@users.noreply.github.com>
2023-04-13 16:18:23 -0700

    data file

---
+++ data.js
@@ -0,0 +1,30 @@
+const myProjects = [
+  {
+    projectNAme: 'Movie Searcher',
+    projectDescription: 'Movie search web app with OMDb API using React.',
+    img: '/web1.png',
+    url: 'https://movie-search-sigma-inky.vercel.app/',
+  },
+  {
+    projectNAme: 'Simple fashion website',
+    projectDescription:
+      'a sleek and simple fashion website built using React and Firebase.',
+    img: '/web6.png',
+    url: 'https://www.heart-fashion.online/',
+  },
+  {
+    projectNAme: 'Doo',
+    projectDescription: 'A to do app Design In Figma',
+    img: '/web2.png',
+    url: 'https://www.figma.com/file/p4UStbukpCR8DYcMWqwlyl/To-Do-App?node-id=0%3A1&t=u8QIOtE7p0tXS24G-1',
+  },
+  {
+    projectNAme: 'Watson Tech Website',
+    projectDescription:
+      'E-commerce web Watson Tech e-commerce site built with Next.js.',
+    img: '/web4.png',
+    url: 'https://watsontechltd.com/',
+  },
+];
+
+export { myProjects };
